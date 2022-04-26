78637d400d45b87039104f630cc7bc5349c8b1fb
Brent Klein <bklein@xometry.com>
2022-04-26 14:50:49 -0400

    added 'inventory' command; added directional shorthands

--- src/main/java/rpg/classes/InputParser.java
+++ src/main/java/rpg/classes/InputParser.java
@@ -2,7 +2,10 @@ package rpg.classes;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InputParser {
 
@@ -17,10 +20,54 @@ public class InputParser {
             Array.set(words,0, actionSet.getVerbOptional().get());
             Array.set(words,1,"room");
             return Optional.of(new ActionSet(words));
-        } else if (actionSet.getSubjectOptional().isPresent()) {
+        } else if (
+            Stream.of(
+                "n", 
+                "north", 
+                "s", 
+                "south", 
+                "e", 
+                "east", 
+                "w", 
+                "west", 
+                "u", 
+                "up", 
+                "d", 
+                "down"
+            ).collect(Collectors.toCollection(HashSet::new))
+            .contains(actionSet.getVerbOptional().get())) {
+                String[] words = new String[2];
+                Array.set(words, 0, "go");
+                Array.set(words, 1, translateDirectionalShorthand(actionSet.getVerbOptional().get()));
+                return Optional.of(new ActionSet(words));
+            } else if (actionSet.getSubjectOptional().isPresent()) {
             return Optional.of(actionSet);
+        } else if (actionSet.getVerbOptional().get().equals("i") || actionSet.getVerbOptional().get().equals("inventory")) {
+            String[] words = new String[2];
+            Array.set(words,0,"look");
+            Array.set(words,1, "inventory");
+            return Optional.of(new ActionSet(words));
         } else {
             return Optional.empty();
+        }
+    }
+
+    private static String translateDirectionalShorthand(String direction) {
+        switch (direction) {
+            case "n":
+                return "north";
+            case "s":
+                return "south";
+            case "e":
+                return "east";
+            case "w":
+                return "west";
+            case "u":
+                return "up";
+            case "d":
+                return "down";
+            default:
+                return direction;
         }
     }
 
--- src/main/java/rpg/classes/Main.java
+++ src/main/java/rpg/classes/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 
 		String filename = "config.json";
 
@@ -16,88 +16,97 @@ public class Main {
 			filename = "simple.example.config.json";
 		}
 
-		State s = WorldBuilder.getNewState(filename).get();
+		State state = WorldBuilder.getNewState(filename).get();
 
-		while (!s.gameOver) {
-			System.out.println(s.currentRoom.getDescription());
-			Map<String,Integer> exits = s.currentRoom.getExits();
+		while (!state.gameOver) {
+			System.out.println(state.currentRoom.getDescription());
+			Map<String, Integer> exits = state.currentRoom.getExits();
 
-			for (String direction :
-					exits.keySet()) {
+			for (String direction : exits.keySet()) {
 				System.out.println("There is an exit to the " + direction.toUpperCase());
 			}
 
-			s.gameOver = s.currentRoom.isEnd();
+			state.gameOver = state.currentRoom.isEnd();
 
-			if (!s.gameOver) {
-				String input = sc.nextLine();
+			if (!state.gameOver) {
+				String input = scanner.nextLine();
 
 				Optional<ActionSet> actionSetOptional = InputParser.parseInput(input);
 
 				actionSetOptional.ifPresentOrElse(
-					// How do we make sure we don't run into collisions between the names of items and features?
-					actionSet -> {
-						Optional<Feature> optionalFeature =
-							s.currentRoom.getFeatures().stream().filter(
-								(f) -> f.getName().equals(actionSet.getSubjectOptional().get())).findAny();
-						Optional<Item> optionalItem = optionalFeature.filter((f) -> f instanceof Item).map((f) -> (Item)f);
-						Optional<Item> optionalInventoryItem = s.player.getItem(actionSet.getSubjectOptional().get());
-						// TODO: refactor to use reflection instead of switch?
-						switch (actionSet.getVerbOptional().get()) {
-							case "go":
-							case "head":
-							case "travel":
+						// How do we make sure we don't run into collisions between the names of items
+						// and features?
+						actionSet -> {
+							Optional<Feature> optionalFeature = state.currentRoom.getFeatures().stream().filter(
+									(f) -> f.getName().equals(actionSet.getSubjectOptional().get())).findAny();
+							Optional<Item> optionalItem = optionalFeature.filter((f) -> f instanceof Item).map((f) -> (Item) f);
+							Optional<Item> optionalInventoryItem = state.player.getItem(actionSet.getSubjectOptional().get());
+							// TODO: refactor to use reflection instead of switch?
+							switch (actionSet.getVerbOptional().get()) {
+								case "go":
+								case "head":
+								case "travel":
 
-								actionSet.getSubjectOptional()
-									.flatMap(s.currentRoom::getAdjacentRoom)
-									.flatMap(s::getRoomOptional)
-									.ifPresentOrElse(
-										(d) -> d.goTo(actionSet, s),
-										() -> System.out.println("There is not an exit in that direction."));
-								break;
-							case "look":
-							case "examine":
-								if (
-									actionSet.getSubjectOptional().get().equalsIgnoreCase("room") ||
-										actionSet.getSubjectOptional().get().equalsIgnoreCase("around")) {
-									s.currentRoom.look(actionSet, s);
-								} else {
-									optionalFeature
-										.or(() -> optionalInventoryItem)
-										.ifPresentOrElse(
-											o -> o.look(actionSet,s),
-											() -> System.out.println("You don't see anything interesting."));
-								}
-								break;
-							case "take":
-								optionalItem.ifPresentOrElse(
-									(item -> item.take(actionSet,s)),
-									() -> System.out.println("You can't take that."));
-								break;
-							case "use":
-								optionalInventoryItem.ifPresentOrElse(
-									(item -> item.use(actionSet,s)),
-									() -> System.out.println("You don't have a " + actionSet.getSubjectOptional().get() + "."));
-								break;
-							case "drop":
-								optionalInventoryItem.ifPresentOrElse(
-									(item -> item.drop(actionSet,s)),
-									() -> System.out.println("You don't have a " + actionSet.getSubjectOptional().get() + "."));
-								break;
-							default:
-								// How do we make sure we don't run into collisions between the names of items and features?
-								optionalFeature.or(() -> optionalInventoryItem)
-									.ifPresentOrElse(
-										(feature -> feature.use(actionSet,s)),
-										() -> System.out.println("You can't do that."));
-								break;
-						}
-					},
-					() -> System.out.println("You can't do that."));
+									actionSet.getSubjectOptional()
+											.flatMap(state.currentRoom::getAdjacentRoom)
+											.flatMap(state::getRoomOptional)
+											.ifPresentOrElse(
+													(d) -> d.goTo(actionSet, state),
+													() -> System.out.println("There is not an exit in that direction."));
+									break;
+								case "look":
+								case "examine":
+									if (actionSet.getSubjectOptional().get().equalsIgnoreCase("room") ||
+											actionSet.getSubjectOptional().get().equalsIgnoreCase("around")) {
+										state.currentRoom.look(actionSet, state);
+									} else if (actionSet.getSubjectOptional().get().equalsIgnoreCase("inventory")) {
+										Set<Item> inventory = state.player.getInventory();
+										if (inventory.size() > 0) {
+											Iterator<Item> inventoryIterator = inventory.iterator();
+											while (inventoryIterator.hasNext()) {
+												System.out.println(inventoryIterator.next().name);
+											}
+										} else {
+											System.out.println("There is nothing in your inventory.");
+										}
+									} else {
+										optionalFeature
+												.or(() -> optionalInventoryItem)
+												.ifPresentOrElse(
+														o -> o.look(actionSet, state),
+														() -> System.out.println("You don't see anything interesting."));
+									}
+									break;
+								case "take":
+									optionalItem.ifPresentOrElse(
+											(item -> item.take(actionSet, state)),
+											() -> System.out.println("You can't take that."));
+									break;
+								case "use":
+									optionalInventoryItem.ifPresentOrElse(
+											(item -> item.use(actionSet, state)),
+											() -> System.out.println("You don't have a " + actionSet.getSubjectOptional().get() + "."));
+									break;
+								case "drop":
+									optionalInventoryItem.ifPresentOrElse(
+											(item -> item.drop(actionSet, state)),
+											() -> System.out.println("You don't have a " + actionSet.getSubjectOptional().get() + "."));
+									break;
+								default:
+									// How do we make sure we don't run into collisions between the names of items
+									// and features?
+									optionalFeature.or(() -> optionalInventoryItem)
+											.ifPresentOrElse(
+													(feature -> feature.use(actionSet, state)),
+													() -> System.out.println("You can't do that."));
+									break;
+							}
+						},
+						() -> System.out.println("You can't do that."));
 			}
 		}
 
 		System.out.println("\n\n\nGame Over!");
-		sc.close();
+		scanner.close();
 	}
 }
